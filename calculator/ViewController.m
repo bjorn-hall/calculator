89e4144e25547d1ef26562cb2edec9d7b180a846
bjorn-hall <bjorn.hall@live.com>
2014-10-04 21:07:16 +0200

    Initial Commit

---
+++ calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  calculator
+//
+//  Created by Björn Hall on 04/10/14.
+//  Copyright (c) 2014 Björn Hall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
